f9a6c4b43ec496f8b5e922ab8c968bfda0b5b4a5
Kylian <dev.kylian@gmail.com>
2019-12-21 15:47:12 +0100

    Update de l'insertion db.sql

--- backend/includes/db.sql
+++ backend/includes/db.sql
@@ -8,17 +8,7 @@ CREATE DATABASE IF NOT EXISTS `ppe2_database` CHARACTER SET utf8 COLLATE utf8_ge
 -- Drop table if exists
 --
 
-DROP TABLE IF EXISTS `roles`, `users`;
-
---
--- Table structure for table `roles`
---
-
-CREATE TABLE `roles` (
-     `role_id` int(11) NOT NULL AUTO_INCREMENT,
-     `role_name` varchar(255) NOT NULL DEFAULT 'user',
-     PRIMARY KEY (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `users`;
 
 --
 -- Table structure for table `users`
@@ -32,9 +22,16 @@ CREATE TABLE `users` (
      `user_email` varchar(255) NOT NULL,
      `user_password` varchar(255) NOT NULL,
      `user_birthdate` varchar(255) NOT NULL,
-     `user_role` int(1) NOT NULL DEFAULT '1',
+     `user_role` int(1) NOT NULL DEFAULT '0',
      `user_reg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
      `user_enabled` tinyint(1) UNSIGNED NOT NULL DEFAULT '1',
      PRIMARY KEY (`user_id`),
      UNIQUE (`user_email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Insert Data for `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_firstname`, `user_lastname`, `user_username`, `user_email`, `user_password`, `user_birthdate`, `user_role`, `user_reg_time`, `user_enabled`) VALUES
+(1, 'demo', 'demo', 'demo', 'demo@kyliandev.fr', '$2y$10$dZrt2T6RHsqRvK221LEqZeS8BBb/Zt4AUCD.0E0sJ.LhRqBPrgNX.', '2019-12-21', 1, '2019-12-21 14:36:14', 1);
